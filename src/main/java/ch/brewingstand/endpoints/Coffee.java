2dae62fd09ec53fc074268fb4c3a92c634b0fc29
IbuprofenLover <pierre.gellet@hes-so.ch>
2026-01-07 16:02:33 +0100

    some minor modifications

--- src/main/java/ch/brewingstand/endpoints/Coffee.java
+++ src/main/java/ch/brewingstand/endpoints/Coffee.java
@@ -2,13 +2,17 @@ package ch.brewingstand.endpoints;
 
 import io.javalin.http.Context;
 
+import java.util.HashMap;
+
 public class Coffee {
     private static int currid = 0;
-    private int id;
+    private final int id;
     private final String name;
     private final int intensity;
     private final String origin;
     private final int bitterness;
+
+    private static HashMap<Integer, Coffee> coffees = new HashMap<Integer, Coffee>();
 
     public Coffee(String name, int intensity, String origin, int bitterness) {
         this.name = name;
@@ -16,11 +20,17 @@ public class Coffee {
         this.origin = origin;
         this.bitterness = bitterness;
         id = currid++;
+        coffees.put(id, this);
     }
 
-    public static void get_coffee(Context ctx) {
-        int idToRetrieve = ctx.pathParamAsClass("coffee_id", Integer.class).get();
-            ctx.result("Tried to get coffee with id " + idToRetrieve);
+    public static void getCoffeeById(Context ctx) {
+        int idToRetrieve = ctx.pathParamAsClass("id", Integer.class).get();
+        ctx.result("coffee with id " + idToRetrieve);
+    }
+
+    public static void getAllCoffees(Context ctx) {
+
+        ctx.result("All coffees");
     }
 
 }
