a1954c78b24bfda1f1d36a79ff906c5bf3ef9277
IbuprofenLover <pierre.gellet@hes-so.ch>
2026-01-07 15:44:05 +0100

    template for manager done. We will need to add the 2 hasmaps and change what happen in the coffee and review function once coffee and review are implemented

--- src/main/java/ch/brewingstand/Main.java
+++ src/main/java/ch/brewingstand/Main.java
@@ -7,8 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
         Javalin app = Javalin.create();
-
-        app.get("/", ctx -> ctx.result("Hello, world!"));
+        RequestManager.delegate(app);
 
         app.start(PORT);
     }
