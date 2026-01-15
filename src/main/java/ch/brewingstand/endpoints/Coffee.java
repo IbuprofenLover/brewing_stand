ec12b2a91c72debc5f1e46994b7fdee1a86f3618
IbuprofenLover <pierre.gellet@hes-so.ch>
2026-01-15 12:06:42 +0100

    added some doc

--- src/main/java/ch/brewingstand/endpoints/Coffee.java
+++ src/main/java/ch/brewingstand/endpoints/Coffee.java
@@ -1,10 +1,9 @@
 package ch.brewingstand.endpoints;
-
-import io.javalin.Javalin;
 import io.javalin.http.Context;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Coffee {
     private static int currid = 0;
@@ -14,14 +13,13 @@ public class Coffee {
     private final int intensity;
     private final int bitterness;
 
-    private static HashMap<Integer, Coffee> coffees = new HashMap<Integer, Coffee>();
+    private static final ConcurrentHashMap<Integer, Coffee> coffees = new ConcurrentHashMap<Integer, Coffee>();
 
     public Coffee(String name, String origin, int intensity, int bitterness) {
         this.name = name;
         this.intensity = Math.clamp(intensity, 1, 10);
         this.origin = origin;
         this.bitterness = bitterness;
-
         id = currid++;
         coffees.put(id, this);
     }
@@ -36,12 +34,18 @@ public class Coffee {
         ctx.json(coffee);
     }
 
+    /**
+     * The function that handle the GET requests, for coffees, using query parameters. it can respond from 0 to all
+     * the coffees
+     *
+     * @param ctx the context of the request
+     */
     public static void getManyCoffees(Context ctx) {
         String nm = ctx.queryParam("name");
         String orgn = ctx.queryParam("origin");
         String bitter = ctx.queryParam("bitterness");
         String intense = ctx.queryParam("intensity");
-        ArrayList<Coffee> returnArray = new ArrayList<Coffee>();
+        List<Coffee> returnArray = new ArrayList<Coffee>();
         if(bitter != null && isInvalidNumeric(bitter, 1, 10)) {
             ctx.result("Error : Bitterness in [1:10]");
             return;
@@ -59,9 +63,16 @@ public class Coffee {
 
             returnArray.add(coffee);
         }
+
         ctx.json(returnArray);
     }
-
+    /**
+     * The function that handle the POST requests for a coffee, given its attributes. The context should include a 4
+     * query parameters : a name, an origin, a bitterness and an intensity. If a coffee already exists with that name,
+     * the coffee won't be able to be created
+     *
+     * @param ctx the context of the request
+     */
     public static void postCoffee(Context ctx) {
 
         String nm = ctx.queryParam("name");
@@ -92,6 +103,12 @@ public class Coffee {
         ctx.json(c);
     }
 
+    /**
+     * The function that handle the DELETE requests on a specific coffee. The context should include
+     * a path parameter named id, which will correspond to the id of the coffee we want to delete.
+     *
+     * @param ctx the context of the request
+     */
     public static void deleteCoffee(Context ctx) {
         Coffee coffee = coffees.get(Integer.parseInt(ctx.pathParam("id")));
         if(coffee == null) {
@@ -111,10 +128,19 @@ public class Coffee {
      * For JSON parsing
      */
     public String getName() {return name;}
+    public int getId() {return id;}
     public String getOrigin() {return origin;}
     public int getIntensity() {return intensity;}
     public int getBitterness() {return bitterness;}
 
+    /**
+     * Takes a string and check if it represents an integer value, and if this value is between the min
+     * and max.
+     * @param str the string we want to verify
+     * @param min the minimal value allowed
+     * @param max the maximal value allowed
+     * @return true if str represents an integer and is between min and max (both included)
+     */
     private static boolean isInvalidNumeric(String str, int min, int max) {
         if (str == null) return true;
         try {
