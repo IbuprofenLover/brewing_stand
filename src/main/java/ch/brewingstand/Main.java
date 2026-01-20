b33b7fe7a1fbe2a4f6fe5583d4eb539ffd24f740
IbuprofenLover <pierre.gellet@hes-so.ch>
2026-01-20 17:58:56 +0100

    added caching strategy

--- src/main/java/ch/brewingstand/Main.java
+++ src/main/java/ch/brewingstand/Main.java
@@ -2,11 +2,16 @@ package ch.brewingstand;
 
 import io.javalin.Javalin;
 
+import java.time.LocalDateTime;
+
 public class Main {
     public static final int PORT = 8080;
 
     public static void main(String[] args) {
-        Javalin app = Javalin.create();
+        Javalin app = Javalin.create(config -> {
+            // This will allow us to parse LocalDateTime
+            config.validation.register(LocalDateTime.class, LocalDateTime::parse);
+        });
         RequestManager.delegate(app);
 
         app.start(PORT);
